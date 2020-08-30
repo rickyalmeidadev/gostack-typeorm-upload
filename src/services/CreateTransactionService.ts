e625cf7664e8c1d178b2dfda028c0904ca9f726a
Ricky Almeida <ricky.almeida.dev@gmail.com>
2020-08-30 15:19:33 -0300

    feat: create business rule for transaction creation

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,10 +1,63 @@
-// import AppError from '../errors/AppError';
+import { getCustomRepository, getRepository } from 'typeorm';
+import AppError from '../errors/AppError';
 
 import Transaction from '../models/Transaction';
+import TransactionsRepository from '../repositories/TransactionsRepository';
+import Category from '../models/Category';
+
+interface RequestDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+  category: string;
+}
 
 class CreateTransactionService {
-  public async execute(): Promise<Transaction> {
-    // TODO
+  public async execute({
+    title,
+    value,
+    type,
+    category,
+  }: RequestDTO): Promise<Transaction> {
+    const transactionsRepository = getCustomRepository(TransactionsRepository);
+    const categoriesRepository = getRepository(Category);
+
+    const { total } = await transactionsRepository.getBalance();
+
+    if (type === 'outcome' && value > total) {
+      throw new AppError('This value extrapolates the total.', 400);
+    }
+
+    const foundCategory = await categoriesRepository.findOne({
+      where: {
+        title: category,
+      },
+    });
+
+    let categoryId: string;
+
+    if (!foundCategory) {
+      const newCategory = categoriesRepository.create({
+        title: category,
+      });
+
+      await categoriesRepository.save(newCategory);
+
+      categoryId = newCategory.id;
+    } else {
+      categoryId = foundCategory.id;
+    }
+
+    const transaction = transactionsRepository.create({
+      title,
+      value,
+      type,
+      category_id: categoryId,
+    });
+
+    await transactionsRepository.save(transaction);
+
+    return transaction;
   }
 }
 
